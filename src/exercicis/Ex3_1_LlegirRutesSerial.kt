63482335c4a67dc83e51ce6e087d6cea01802c88
Cherry708 <aluser8241@ieselcaminas.org>
2021-10-04 16:49:09 +0200

    Initial commit

---
+++ src/exercicis/Ex3_1_LlegirRutesSerial.kt
@@ -0,0 +1,25 @@
+package exercicis
+
+import java.io.FileInputStream
+import java.io.DataInputStream
+
+fun main() {
+    val f = DataInputStream(FileInputStream("Rutes.dat"))
+
+    while (f.available() > 0) {
+        System.out.println("Ruta: " + f.readUTF())
+        System.out.println("Desnivell: " + f.readInt())
+        System.out.println("Desnivell acumulat: " + f.readInt())
+
+        val punts = f.readInt()
+        System.out.println("Te $punts punts")
+        for (i in 1..punts){
+            print("Punt ${i}: ")
+            print(f.readUTF()+" ")
+            print("(${f.readDouble()}, ${f.readDouble()})")
+            println()
+        }
+        System.out.println()
+    }
+    f.close()
+}
